dd189a5879789f52f76eca37708bca23589e4932
Yashdev-ops <ydustakar@gmail.com>
2024-12-25 18:09:20 +0530

    sfds

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -1,11 +1,11 @@
-# Fetch the peer VPC (Ansible VPC)
-data "aws_vpc" "ansible_vpc" {
-  id = "vpc-0b1be6d8fbd694712" # Replace with actual peer VPC ID
+# Fetch the route table for the current VPC (public route table)
+data "aws_route_table" "terraform_public" {
+  vpc_id = data.aws_vpc.default_vpc.id # Replace with your default VPC's ID
 }
 
-# Fetch the default VPC (the current VPC)
-data "aws_vpc" "default_vpc" {
-  id = "vpc-xxxxxxxx" # Replace with the actual default VPC ID
+# Fetch the route table for the peer VPC (Ansible VPC route table)
+data "aws_route_table" "ansible_vpc_rt" {
+  vpc_id = data.aws_vpc.ansible_vpc.id # Replace with your peer VPC's ID
 }
 
 # Resource: VPC Peering Connection
@@ -29,7 +29,6 @@ resource "aws_vpc_peering_connection" "ansible_vpc_peering" {
 
 # Route: Add a route from the current VPC to the peer VPC
 resource "aws_route" "peering_to_ansible_vpc" {
-  count                     = length(data.aws_route_table.terraform_public.routes) > 0 ? 0 : 1
   route_table_id            = data.aws_route_table.terraform_public.id
   destination_cidr_block    = "10.37.0.0/16" # Route to peer VPC CIDR block
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
@@ -39,7 +38,6 @@ resource "aws_route" "peering_to_ansible_vpc" {
 
 # Route: Add a route from the peer VPC to the current VPC
 resource "aws_route" "peering_from_ansible_vpc" {
-  count                     = length(data.aws_route_table.ansible_vpc_rt.routes) > 0 ? 0 : 1
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
   destination_cidr_block    = "172.31.0.0/16" # Route to current VPC CIDR block
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
