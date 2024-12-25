fed3ce011f92aa6ff44c3339baea0dd986c81165
Yashdev-ops <ydustakar@gmail.com>
2024-12-25 17:55:56 +0530

    ad\

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -8,20 +8,14 @@ data "aws_vpc" "default_vpc" {
   id = aws_vpc.default.id
 }
 
-# Fetch the route table for the current VPC (by vpc_id and filter by tag Name)
+# Directly reference the known route table ID for the current VPC
 data "aws_route_table" "terraform_public" {
-  vpc_id = data.aws_vpc.default_vpc.id
-
-  # Using the filter to match the route table tag Name="public-route-table"
-  filter {
-    name   = "tag:Name"
-    values = ["public-route-table"]
-  }
+  route_table_id = "rtb-0c3d380cc4a1d7339" # Known route table ID, based on your describe command
 }
 
 # Fetch the route table for the peer VPC (using the known route table ID)
 data "aws_route_table" "ansible_vpc_rt" {
-  route_table_id = "rtb-0c3d380cc4a1d7339" # Known route table ID from the describe command
+  route_table_id = "rtb-0c3d380cc4a1d7339" # Replace with the actual route table ID
 }
 
 # Resource: VPC Peering Connection
@@ -46,7 +40,7 @@ resource "aws_vpc_peering_connection" "ansible_vpc_peering" {
 # Route: Add a route from the current VPC to the peer VPC
 resource "aws_route" "peering_to_ansible_vpc" {
   route_table_id            = data.aws_route_table.terraform_public.id
-  destination_cidr_block    = "172.31.0.0/16" # Adjust CIDR block as per peer VPC's CIDR
+  destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
 
   depends_on = [aws_vpc_peering_connection.ansible_vpc_peering]
@@ -55,7 +49,7 @@ resource "aws_route" "peering_to_ansible_vpc" {
 # Route: Add a route from the peer VPC to the current VPC
 resource "aws_route" "peering_from_ansible_vpc" {
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-  destination_cidr_block    = "10.37.0.0/16" # Adjust CIDR block as per current VPC's CIDR
+  destination_cidr_block    = "10.37.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
 
   depends_on = [aws_vpc_peering_connection.ansible_vpc_peering]
