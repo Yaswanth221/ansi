566c9ad32d16765616aa36b1779a17dbb6721e58
Yashdev-ops <ydustakar@gmail.com>
2024-12-25 16:48:33 +0530

    added

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -3,17 +3,13 @@ data "aws_vpc" "ansible_vpc" {
   id = "vpc-0b1be6d8fbd694712"
 }
 
-# Fetch the route table using subnet_id, if it exists
+# Fetch the route table using its ID
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-0d410dd41581e1303"
+  route_table_id = "rtb-0c3d380cc4a1d7339"
 }
 
-# data "aws_route_table" "ansible_vpc_rt" {
-#   vpc_id = "vpc-0b1be6d8fbd694712"
-# }
-
 # Resource: VPC Peering Connection
-resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
+resource "aws_vpc_peering_connection" "ansible_vpc_peering" {
   peer_vpc_id = data.aws_vpc.ansible_vpc.id
   vpc_id      = aws_vpc.default.id
   auto_accept = true
@@ -32,24 +28,21 @@ resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
 }
 
 # Route: Add a route from the current VPC to the peer VPC
-resource "aws_route" "peering-to-ansible-vpc" {
-  route_table_id            = aws_route_table.terraform-public.id
+resource "aws_route" "peering_to_ansible_vpc" {
+  route_table_id            = aws_route_table.terraform_public.id
   destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
 
   # Ensure the VPC peering connection exists before creating this route
-  depends_on = [aws_vpc_peering_connection.ansible-vpc-peering]
+  depends_on = [aws_vpc_peering_connection.ansible_vpc_peering]
 }
 
 # Route: Add a route from the peer VPC to the current VPC
-resource "aws_route" "peering-from-ansible-vpc" {
-  # Dynamically fetch route table ID if it exists, otherwise handle gracefully
-  count = length([for rt in data.aws_route_table.ansible_vpc_rt : rt.id if rt.id != null])
-
-  route_table_id            = data.aws_route_table.ansible_vpc_rt[count.index].id
+resource "aws_route" "peering_from_ansible_vpc" {
+  route_table_id            = data.aws_route_table.ansible_vpc_rt.id
   destination_cidr_block    = "10.37.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.ansible_vpc_peering.id
 
   # Ensure the VPC peering connection exists before creating this route
-  depends_on = [aws_vpc_peering_connection.ansible-vpc-peering]
+  depends_on = [aws_vpc_peering_connection.ansible_vpc_peering]
 }
