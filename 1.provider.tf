de7bbecc56dc6ae3a125eb2532dedf5cfb0cead3
Yaswanth <ydustakar@gmail.com>
2025-01-21 19:38:02 +0530

    Update 1.provider.tf

--- 1.provider.tf
+++ 1.provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "mybucket0000001"
+    bucket = "mybucket000000123"
     key    = "Ansible.tfstate"
     region = "us-east-1"
     # dynamodb_table = "-terraform-locks"
